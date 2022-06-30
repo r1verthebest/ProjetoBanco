c5256094096edc8c4a9225d6caec6b7132bd72cb
r1verthebest <69735602+r1verthebest@users.noreply.github.com>
2022-06-30 13:56:52 -0300

    Add files via upload

---
+++ src/projetobanco/Poupanca.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetobanco;
+
+public class Poupanca extends Conta {
+    
+    public void debito(double valor) {
+        
+        this.saldo = this.saldo - valor;
+        
+    }
+    
+    public String toString() {
+        return "Conta Poupança:\nNúmero: " + Integer.toString(this.agencia) + 
+                "\nConta: " + Integer.toString(this.numero) +
+                "\nSaldo: " + Double.toString(this.saldo) + "\n-------------";
+    }
+    
+}
